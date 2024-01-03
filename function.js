95bd1e2758902fdd778e023828b7e4195483da47
Uki <in2cr7@gmail.com>
2024-01-03 23:15:15 +0530

    W3 func

---
+++ function.js
@@ -0,0 +1,41 @@
+// var o = mine(3,4);
+// function mine(s,t){
+//     return s*t;
+// }
+
+function hara(){
+console.log("hi there")
+}
+hara()
+
+var j = 2;
+var k = 3;
+ 
+function add(){
+    console.log(j+k)
+}
+add()
+
+var factor = "Surya"
+var fmovie = "Rolex"
+
+function favourite(){
+  console.log("favourite actor is:"+ factor)
+  console.log("favourite movie is:"+fmovie)
+}
+  favourite()
+
+  function bd(a,b){
+    console.log(a+b)
+  }
+  bd(5,6)
+
+
+function area(l,b){
+    var a = l*b
+    console.log("area is:"+a)
+}
+var length=10
+var breadth=15
+
+area(10,15)
